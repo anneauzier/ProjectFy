4c4654d0aba425a516379b23e7638914cf85a957
anneauzier <anne.auziier@outlook.com>
2023-07-26 20:14:48 -0400

    gitignore added

---
+++ ProjectFy/ProjectFyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectFyApp.swift
+//  ProjectFy
+//
+//  Created by Anne Victoria Batista Auzier on 26/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectFyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
