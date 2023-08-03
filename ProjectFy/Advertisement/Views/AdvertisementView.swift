054093f047755f799cf58310b7aef12fd6960aae
iagoramoss <mirandolaiago@gmail.com>
2023-08-03 09:42:45 -0400

    salvando pra mudar de mac

---
+++ ProjectFy/Advertisement/Views/AdvertisementView.swift
@@ -0,0 +1,20 @@
+//
+//  AdvertisementView.swift
+//  ProjectFy
+//
+//  Created by Iago Ramos on 31/07/23.
+//
+
+import SwiftUI
+
+struct AdvertisementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AdvertisementView_Previews: PreviewProvider {
+    static var previews: some View {
+        AdvertisementView()
+    }
+}
