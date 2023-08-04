77e5640f699b467c8d9f7b70728f7375daefe425
anneauzier <anne.auziier@outlook.com>
2023-08-04 00:55:44 -0400

    feature: User profile + voiceOver

--- ProjectFy/User/View/EditUserView.swift
+++ ProjectFy/User/View/EditUserView.swift
@@ -19,74 +19,82 @@ struct EditUserView: View {
     }
     
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                Button {
-                    dismiss()
-                } label: {
-                    Text("Cancelar")
-                        .font(.subheadline)
+        ScrollView {
+            VStack(alignment: .leading) {
+                HStack {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("Cancelar")
+                            .font(.subheadline)
+                            .foregroundColor(.black)
+                            .bold()
+                    }
+                    Spacer()
+                    
+                    Text("Editar perfil")
                         .foregroundColor(.black)
-                        .bold()
-                }
-                Spacer()
+                        .font(.headline)
+                    
+                    Spacer()
+                    
+                    Button {
+                        viewModel.saveChanges(editedUser: editingUser)
+                        dismiss()
+                    } label: {
+                        Text("Salvar")
+                            .font(.subheadline)
+                            .foregroundColor(.black)
+                            .bold()
+                    }
+                }.padding(.top, 20)
                 
-                Text("Editar perfil")
-                    .foregroundColor(.black)
-                    .font(.headline)
-                
-                Spacer()
-                
-                Button {
-                    viewModel.saveChanges(editedUser: editingUser)
-                    dismiss()
-                } label: {
-                    Text("Salvar")
-                        .font(.subheadline)
-                        .foregroundColor(.black)
-                        .bold()
-                }
-            }.padding(.top, 20)
- 
-            Group {
-                Text("Nome")
-                    .font(.headline)
-                    .foregroundColor(.gray)
-                TextField("Digite aqui seu nome", text: $editingUser.name)
-                    .textFieldStyle(.roundedBorder)
-                
-                Text("Área de Conhecimento")
-                    .font(.headline)
-                    .foregroundColor(.gray)
-                TextField("Digite aqui sua área de interesse(Ex: UI/UX, etc)",
-                          text: $editingUser.areaExpertise)
-                .textFieldStyle(.roundedBorder)
-                
-                DropDownButton(viewModel: viewModel, editingUser: $editingUser)
-                
-                Text("Localização")
-                    .font(.headline)
-                    .foregroundColor(.gray)
-                TextField("State, Country", text: $editingUser.region)
-                    .textFieldStyle(.roundedBorder)
-                
-                Text("Interesses")
-                    .font(.headline)
-                    .foregroundColor(.gray)
-                TextField("Seus interesses", text: $editingUser.interestTags)
-                    .textFieldStyle(.roundedBorder)
-                
-                Toggle(isOn: $editingUser.available) {
-                    Text("Disponibilidade")
+                Group {
+                    Text("Nome")
                         .font(.headline)
                         .foregroundColor(.gray)
-                }
+                        .accessibilityLabel("Seu nome")
 
-            }.padding(.top, 10)
-
-            Spacer()
-            
-        }.padding(.horizontal, 20)
+                    TextField("Digite aqui seu nome", text: $editingUser.name)
+                        .textFieldStyle(.roundedBorder)
+                        .accessibilityLabel("Digite aqui seu nome")
+                    
+                    Text("Área de Conhecimento")
+                        .font(.headline)
+                        .foregroundColor(.gray)
+                    TextField("Digite aqui sua área de interesse(Ex: UI/UX, etc)",
+                              text: $editingUser.areaExpertise)
+                    .textFieldStyle(.roundedBorder)
+                    .accessibilityLabel("Digite aqui sua área de interesse")
+                    
+                    DropDownButton(viewModel: viewModel, editingUser: $editingUser)
+                    
+                    Text("Localização")
+                        .font(.headline)
+                        .foregroundColor(.gray)
+                    TextField("State, Country", text: $editingUser.region)
+                        .textFieldStyle(.roundedBorder)
+                        .accessibilityLabel("Digite seu estado, depois país")
+                    
+                    Text("Interesses")
+                        .font(.headline)
+                        .foregroundColor(.gray)
+                    TextField("Seus interesses", text: $editingUser.interestTags)
+                        .textFieldStyle(.roundedBorder)
+                        .accessibilityLabel("Digite seus interesses")
+                    
+                    Toggle(isOn: $editingUser.available) {
+                        Text("Disponibilidade")
+                            .font(.headline)
+                            .foregroundColor(.gray)
+                    }
+                    
+                }.padding(.top, 10)
+                
+                Spacer()
+                
+            }.padding(.horizontal, 20)
+        }
     }
 }
 
@@ -98,9 +106,7 @@ struct EditUserView_Previews: PreviewProvider {
 }
 
 struct DropDownButton: View {
-
     @ObservedObject var viewModel: UserViewModel
-    @State var selectedExpertise = ""
     @Binding var editingUser: User
 
     let expertise = User.Expertise.allCases
@@ -129,9 +135,9 @@ struct DropDownButton: View {
             RoundedRectangle(cornerRadius: 8)
                 .padding()
                 .foregroundColor(.clear)
-                .border(Color.gray.opacity(0.2))
+                .border(Color.secondary.opacity(0.18))
         }
-            
+        .accessibilityLabel("Escolha seu nível de conhecimento")
     }
 }
 
--- ProjectFy/User/View/UserView.swift
+++ ProjectFy/User/View/UserView.swift
@@ -21,12 +21,14 @@ struct UserView: View {
                         .font(.headline)
                         .foregroundColor(.gray)
                         .frame(maxWidth: .infinity, alignment: .trailing)
+                        .accessibilityLabel("Edital Perfil")
 
                 }.sheet(isPresented: $goEditUserView, content: {
                     EditUserView(viewModel: viewModel)
                 })
                 Image("\(viewModel.user.avatar)")
                     .aspectRatio(contentMode: .fit)
+                    .accessibilityLabel("Foto de perfil")
             }
             
             Divider()
@@ -42,9 +44,12 @@ struct UserView: View {
                     Text("\(viewModel.user.name)")
                         .font(.title)
                         .bold()
+                        .accessibilityLabel("Username \(viewModel.user.name)")
+
                     Text("\(User.mock[0].username)")
                         .foregroundColor(.gray)
                         .bold()
+                        .accessibilityLabel("@\(User.mock[0].username)")
                 }
                 
                 HStack {
@@ -58,6 +63,7 @@ struct UserView: View {
                     Text("\(viewModel.user.expertise.rawValue)")
                         .foregroundColor(.gray)
                         .bold()
+                        .accessibilityLabel("Nível de Conhecimento \(viewModel.user.expertise.rawValue)")
                 }
                 
                 HStack {
@@ -65,7 +71,8 @@ struct UserView: View {
                     Text("\(viewModel.user.region)")
                         .foregroundColor(.gray)
                         .bold()
-                }
+                }.accessibilityElement(children: .combine)
+                .accessibilityLabel("Região \(viewModel.user.region)")
             }
             
             Divider()
@@ -84,6 +91,7 @@ struct UserView: View {
                             .padding(7)
                             .foregroundColor(.white)
                             .bold(true)
+                            .lineLimit(0)
                             .background(Capsule().fill(.gray))
                     }
                 }
